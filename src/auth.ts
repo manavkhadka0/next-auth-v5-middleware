f2e654bc1281dbc5040c3cb3ba18c64eb66d1fd9
Manav Khadka <manavkhadka0@gmail.com>
2024-06-19 20:41:06 +0545

    dsada

--- src/auth.ts
+++ src/auth.ts
@@ -1,38 +1,55 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-import Credentials from 'next-auth/providers/credentials';
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+
+const dummy_user = {
+  id: 1,
+  name: "test user",
+  email: "admin@admin.com",
+  password: "admin",
+};
 
 async function getUser(email: string, password: string): Promise<any> {
-	return {
-		id: 1,
-		name: 'test user',
-		email: email,
-		password: password,
-	};
+  if (email !== dummy_user.email || password !== dummy_user.password) {
+    return null;
+  }
+  return {
+    id: 1,
+    name: "test user",
+    email: email,
+    password: password,
+  };
 }
 
 export const {
-	auth,
-	signIn,
-	signOut,
-	handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
 } = NextAuth({
-	...authConfig,
-	providers: [
-		Credentials({
-			name: 'credentials',
-			credentials: {
-				email: { label: 'email', type: 'text' },
-				password: { label: 'password', type: 'password' },
-			},
-			async authorize(credentials) {
-				const user = await getUser(
-					credentials.email as string,
-					credentials.password as string
-				);
+  ...authConfig,
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.email !== dummy_user.email ||
+          credentials.password !== dummy_user.password
+        ) {
+          return null;
+        }
 
-				return user ?? null;
-			},
-		}),
-	],
+        const user = await getUser(
+          credentials.email as string,
+          credentials.password as string
+        );
+
+        return user ?? null;
+      },
+    }),
+  ],
 });
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,37 +1,38 @@
-'use client';
+"use client";
 
-import { login } from '@/lib/actions';
-import { useFormState } from 'react-dom';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+import { login } from "@/lib/actions";
+import { useFormState } from "react-dom";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const loginInitialState = {
-	message: '',
-	errors: {
-		email: '',
-		password: '',
-		credentials: '',
-		unknown: '',
-	},
+  message: "",
+  errors: {
+    email: "",
+    password: "",
+    credentials: "",
+    unknown: "",
+  },
 };
 
 const Form = () => {
-	const [formState, formAction] = useFormState(login, loginInitialState);
+  const [formState, formAction] = useFormState(login, loginInitialState);
 
-	return (
-		<form action={formAction} className='space-y-4 w-full max-w-sm'>
-			<Input required name='email' placeholder='email' />
-			<Input
-				required
-				name='password'
-				type='password'
-				placeholder='password'
-			/>
-			<Button variant='secondary' className='w-full' type='submit'>
-				submit
-			</Button>
-		</form>
-	);
+  return (
+    <form action={formAction} className="space-y-4 w-full max-w-sm">
+      <Input required name="email" placeholder="email" />
+      {formState.errors.email && (
+        <p className="text-red-600">{formState.errors.email}</p>
+      )}
+      <Input required name="password" type="password" placeholder="password" />
+      {formState.errors.password && (
+        <p className="text-red-600">{formState.errors.password}</p>
+      )}
+      <Button variant="secondary" className="w-full" type="submit">
+        submit
+      </Button>
+    </form>
+  );
 };
 
 export default Form;
